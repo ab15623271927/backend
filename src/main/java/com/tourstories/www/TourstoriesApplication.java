8ea4c75886e34b506cd1578b9ae24aa6a877e19b
Sajib Chakma <chakma.sajib95@yahoo.com>
2020-09-01 21:22:45 +0800

    setup spring boot

---
+++ src/main/java/com/tourstories/www/TourstoriesApplication.java
@@ -0,0 +1,13 @@
+package com.tourstories.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourstoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourstoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourstories/www/TourstoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourstories.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourstoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
